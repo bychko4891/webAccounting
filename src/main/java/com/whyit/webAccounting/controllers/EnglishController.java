8ff564950aa35e1ee96797db9ca65229653d6756
Anatolii <106445100+bychko4891@users.noreply.github.com>
2023-03-27 15:47:01 +0300

    one version

--- src/main/java/com/whyit/webAccounting/controllers/EnglishController.java
+++ src/main/java/com/whyit/webAccounting/controllers/EnglishController.java
@@ -1,27 +1,116 @@
 package com.whyit.webAccounting.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.whyit.webAccounting.English;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 @Controller
 public class EnglishController {
-  English engl1 = new English();
-  English engl2 = new English();
+
+
     @GetMapping("/english")
     public String greeting(Model model) {
-        String str1 = engl1.test();
-        String str2 = engl2.test2();
+        English english = new English();
+//        String parameter = request.getParameter("name");
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            String jsonString = mapper.writeValueAsString(english);
+//            System.out.println(jsonString);
+//        } catch (Exception e) {
+//            System.out.println("Json Error");
+//        }
+        model.addAttribute(english.getUkrText());
         model.addAttribute("title", "English");
-        model.addAttribute("Ukrainian_text", str1);
-        model.addAttribute("English_text", str2);
+        model.addAttribute("Ukrainian_text", english.getUkrText());
+        model.addAttribute("English_text", english.getEngText());
         return "english";
     }
 
+    //    @GetMapping(value ="/eng", params = "name")
+//    @ResponseStatus(HttpStatus.CONFLICT)
+//    @ResponseBody
+//    public String response2(@RequestParam(value = "name", required = false)String name){
+//        String jsonString = "";
+//        English english = new English();
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            jsonString = mapper.writeValueAsString(english);
+//            System.out.println(jsonString);
+//            return jsonString;
+//        } catch (Exception e) {
+//            jsonString = "Json Error";
+//        }
+//        return jsonString;
+//    }
+//    @GetMapping(value ="/english2", params = "name")
+//    @ResponseStatus(HttpStatus.CONFLICT)
+//    @ResponseBody
+//    public void response2(HttpServletRequest request,HttpServletResponse response) throws IOException {
+//        PrintWriter printWriter = response.getWriter();
+//
+//        String jsonString = "";
+//        English english = new English();
+//        ObjectMapper mapper = new ObjectMapper();
+//        try {
+//            jsonString = mapper.writeValueAsString(english);
+//            System.out.println(jsonString);
+////            return jsonString;
+//        } catch (Exception e) {
+//            jsonString = "Json Error";
+//        }
+//        printWriter.print(jsonString);
+//        printWriter.close();
+////        return "english";
+//    }
+    @GetMapping(path = "/englishJSON")
+    public String response2(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        PrintWriter printWriter = response.getWriter();
 
+        String jsonString = "";
+        English english = new English();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            jsonString = mapper.writeValueAsString(english);
+//            return jsonString;
+        } catch (Exception e) {
+            jsonString = "Json Error";
+        }
+        printWriter.println(jsonString);
+        printWriter.close();
+        return "english";
+    }
+//    @RequestMapping(path = "/englishGSON", method = RequestMethod.GET)
+//    public String resp(@RequestParam(value = "name", required = false) String name) {
+////        PrintWriter printWriter = response.getWriter();
+////        response.setContentType("text/html");
+////        String jsonString = "";
+////        English english = new English();
+////        ObjectMapper mapper = new ObjectMapper();
+////        try {
+////            jsonString = mapper.writeValueAsString(english);
+//////            System.out.println(jsonString);
+//////            return jsonString;
+////        } catch (Exception e) {
+////            jsonString = "Json Error";
+////        }
+////        printWriter.println(jsonString);
+////        System.out.println(jsonString);
+////        printWriter.close();
+//        return "english222";
+//    }
 
 }
